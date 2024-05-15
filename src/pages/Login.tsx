50bf04f5f42f9033956c2fecb766dbc7752c704a
ARYAN PATEL <patelaryan7751@gmail.com>
2024-05-16 03:52:16 +0530

    minor change

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,11 +35,6 @@ export default function Login() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center py-36 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          {/* <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          /> */}
           <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Self-Service Data Platform
           </h2>
