83e6de631223aa35ab8e4c0833aa5098ae630b35
ARYAN PATEL <patelaryan7751@gmail.com>
2024-05-15 19:28:05 +0530

    initialization of the project

---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <>
+      <h1>Welcome Page</h1>
+    </>
+  );
+};
+
+export default WelcomePage;
---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import WelcomePage from "../pages/WelcomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  // Protected Routes
+  //   {
+  //     path: "account",
+  //     element: <AccountLayout />
+  //   },
+]);
+export default router;
